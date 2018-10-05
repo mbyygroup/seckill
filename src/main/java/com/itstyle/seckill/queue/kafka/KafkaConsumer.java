d8ae57c226133998667e7b76c7eed10691da0dfb
mbyy <17600733846@163.com>
2018-10-05 22:30:57 +0800

    修改了pom说明

---
+++ src/main/java/com/itstyle/seckill/queue/kafka/KafkaConsumer.java
@@ -0,0 +1,35 @@
+package com.itstyle.seckill.queue.kafka;
+
+import com.itstyle.seckill.controller.config.RedisUtil;
+import com.itstyle.seckill.controller.config.WebSocketServer;
+import com.itstyle.seckill.pojo.Result;
+import com.itstyle.seckill.service.ISeckillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/*消费者队列*/
+@Component
+public class KafkaConsumer {
+    @Autowired
+    private ISeckillService seckillService;
+
+    private static RedisUtil redisUtil=new RedisUtil();
+    /*
+    *
+    * 监听seckill主题，有消息就读取
+    * */
+    @KafkaListener(topics = {"seckill"})
+    public void receiveMessage(String message){
+        //收到通道消息后就执行秒杀操作
+        String[] array=message.split(":");
+        Result result=seckillService.startSeckill(Long.parseLong(array[0]),Long.parseLong(array[1]));
+        if (result.equals(Result.ok())){
+            WebSocketServer.sendInfo(array[0].toString(),"秒杀成功");
+        }else {
+            WebSocketServer.sendInfo(array[0].toString(),"秒杀失败");
+            redisUtil.cacheValue(array[0],"ok");
+        }
+    }
+
+}
